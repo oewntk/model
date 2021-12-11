e8a05eea5286aed3f47428a8a6c7bec1c19d37f9
1313ou <1313ou@gmail.com>
2021-12-11 12:00:37 +0100

    enhanced tests

--- src/test/java/org/oewntk/model/LibTestModelKeys.java
+++ src/test/java/org/oewntk/model/LibTestModelKeys.java
@@ -6,204 +6,181 @@ package org.oewntk.model;
 
 import java.io.PrintStream;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 
 public class LibTestModelKeys
 {
-	public static void testEarth(final CoreModel model, final PrintStream ps)
+	public static void testEarthMulti(final CoreModel model, final PrintStream ps)
 	{
-		String lemma = "Earth";
 		Pronunciation pEarthGB = Pronunciation.ipa("ɜːθ", "GB");
 		Pronunciation pEarthUS = Pronunciation.ipa("ɝθ", "US");
-		dump(lemma, 'n', 'n', model, ps);
+		Key.IC key_uc = new Key.IC("Earth", 'n', pEarthGB, pEarthUS);
+		Key.IC key_lc = new Key.IC("earth", 'n', pEarthGB, pEarthUS);
+		Key.IC key_pwn_uc = new Key.IC("Earth", 'n', pEarthGB, pEarthUS);
+		Key.IC key_pwn_lc = new Key.IC("earth", 'n', pEarthGB, pEarthUS);
 
-		ps.println();
-
-		Key.IC key_ic = new Key.IC(lemma, 'n', pEarthGB, pEarthUS);
-		ps.println(key_ic.toLongString());
-		Lex[] lexes_ic = key_ic.apply(model.lexesByLemma);
-		for (var lex : lexes_ic)
-		{
-			ps.println("\t" + lex);
-		}
-
-		Key.IC key_pwn = new Key.IC(lemma, 'n', pEarthGB, pEarthUS);
-		ps.println(key_pwn.toLongString());
-		Lex[] lexes_pwn = key_pwn.apply(model.lexesByLemma);
-		for (var lex : lexes_pwn)
-		{
-			ps.println("\t" + lex);
-		}
+		testLemmaKeysMulti(model, ps, "Earth", 'n', 'n', key_uc, key_lc, key_pwn_uc, key_pwn_lc);
 	}
 
-	public static void testBaroque(final CoreModel model, final PrintStream ps)
+	public static void testEarthMono(final CoreModel model, final PrintStream ps)
 	{
-		String lemma = "baroque";
+		Pronunciation pEarthGB = Pronunciation.ipa("ɜːθ", "GB");
+		Pronunciation pEarthUS = Pronunciation.ipa("ɝθ", "US");
+		Key.OEWN key_uc = new Key.OEWN("Earth", 'n', pEarthGB, pEarthUS);
+		Key.OEWN key_lc = new Key.OEWN("earth", 'n', pEarthGB, pEarthUS);
+
+		testLemmaKeysMono(model, ps, "Earth", 'n', 'n', key_uc, key_lc);
+	}
+
+	public static void testBaroqueMulti(final CoreModel model, final PrintStream ps)
+	{
 		Pronunciation pBaroqueGB = Pronunciation.ipa("bəˈɹɒk", "GB");
 		Pronunciation pBaroqueUS = Pronunciation.ipa("bəˈɹoʊk", "US");
+		Key.IC key1_ic = new Key.IC("baroque", 'a', pBaroqueGB, pBaroqueUS);
+		Key.IC key2_ic = new Key.IC("baroque", 's', pBaroqueGB, pBaroqueUS);
+		Key.IC key3_ic = new Key.IC("baroque", 'a', pBaroqueUS, pBaroqueGB);
+		Key.IC key4_ic = new Key.IC("Baroque", 'a', pBaroqueUS, pBaroqueGB);
 
-		dump(lemma, 'a', 's', model, ps);
+		testLemmaKeysMulti(model, ps, "Baroque", 'a', 's', key1_ic, key2_ic, key3_ic, key4_ic);
+	}
 
-		Key.IC key1_ic = new Key.IC(lemma, 'a', pBaroqueGB, pBaroqueUS);
-		ps.println(key1_ic.toLongString());
-		Lex[] lexes1 = key1_ic.apply(model.lexesByLemma);
-		for (var lex : lexes1)
-		{
-			ps.println("\t" + lex);
-		}
+	public static void testBaroqueMono(final CoreModel model, final PrintStream ps)
+	{
+		Pronunciation pBaroqueGB = Pronunciation.ipa("bəˈɹɒk", "GB");
+		Pronunciation pBaroqueUS = Pronunciation.ipa("bəˈɹoʊk", "US");
+		Key.OEWN key_uc = new Key.OEWN("Baroque", 'a', pBaroqueUS, pBaroqueGB);
+		Key.OEWN key_lc = new Key.OEWN("baroque", 'a', pBaroqueGB, pBaroqueUS);
 
-		Key.IC key2_ic = new Key.IC(lemma, 's', pBaroqueGB, pBaroqueUS);
-		ps.println(key2_ic.toLongString());
-		Lex[] lexes2 = key2_ic.apply(model.lexesByLemma);
-		for (var lex : lexes2)
-		{
-			ps.println("\t" + lex);
-		}
-
-		Key.IC key3_ic = new Key.IC(lemma, 'a', pBaroqueUS, pBaroqueGB);
-		ps.println(key3_ic.toLongString());
-		Lex[] lexes3 = key1_ic.apply(model.lexesByLemma);
-		for (var lex : lexes3)
-		{
-			ps.println("\t" + lex);
-		}
-
-		lemma = "Baroque";
-		Key.IC key4_ic = new Key.IC(lemma, 'a', pBaroqueUS, pBaroqueGB);
-		ps.println(key4_ic.toLongString());
-		Lex[] lexes4 = key1_ic.apply(model.lexesByLemma);
-		for (var lex : lexes4)
-		{
-			ps.println("\t" + lex);
-		}
+		testLemmaKeysMono(model, ps, "Baroque", 'a', 's', key_uc, key_lc);
 	}
 
 	public static void testMobile(final CoreModel model, final PrintStream ps)
 	{
-		String lemma = "mobile";
 		Pronunciation pMobileGB = Pronunciation.ipa("ˈməʊbaɪl", "GB");
 		Pronunciation pMobileUS = Pronunciation.ipa("ˈmoʊbil", "US");
-		dump(lemma, 'n', 'n', model, ps);
 
-		Key.IC key1_ic = new Key.IC(lemma, 'n', pMobileGB, pMobileUS);
-		ps.println(key1_ic.toLongString());
-		Lex[] lexes1 = key1_ic.apply(model.lexesByLemma);
-		for (var lex : lexes1)
-		{
-			ps.println("\t" + lex);
-		}
+		Key.IC key1_ic = new Key.IC("mobile", 'n', pMobileGB, pMobileUS);
+		Key.IC key2_ic = new Key.IC("mobile", 'n', pMobileGB, pMobileUS);
+		Key.IC key1u_ic = new Key.IC("Mobile", 'n', pMobileGB, pMobileUS);
+		Key.IC key2u_ic = new Key.IC("Mobile", 'n', pMobileGB, pMobileUS);
+		Key.IC key3u_ic = new Key.IC("MOBILE", 'n', pMobileGB, pMobileUS);
 
-		Key.IC key2_ic = new Key.IC(lemma, 'n', pMobileGB, pMobileUS);
-		ps.println(key2_ic.toLongString());
-		Lex[] lexes2 = key2_ic.apply(model.lexesByLemma);
-		for (var lex : lexes2)
-		{
-			ps.println("\t" + lex);
-		}
-
-		lemma = "Mobile";
-		Key.IC key1u_ic = new Key.IC(lemma, 'n', pMobileGB, pMobileUS);
-		ps.println(key1u_ic.toLongString());
-		Lex[] lexes1u = key1u_ic.apply(model.lexesByLemma);
-		for (var lex : lexes1u)
-		{
-			ps.println("\t" + lex);
-		}
-
-		Key.IC key2u_ic = new Key.IC(lemma, 'n', pMobileGB, pMobileUS);
-		ps.println(key2u_ic.toLongString());
-		Lex[] lexes2u = key2u_ic.apply(model.lexesByLemma);
-		for (var lex : lexes2u)
-		{
-			ps.println("\t" + lex);
-		}
-
-		lemma = "MOBILE";
-		Key.IC key3u_ic = new Key.IC(lemma, 'n', pMobileGB, pMobileUS);
-		ps.println(key3u_ic.toLongString());
-		Lex[] lexes3u = key1u_ic.apply(model.lexesByLemma);
-		for (var lex : lexes3u)
-		{
-			ps.println("\t" + lex);
-		}
+		testLemmaKeysMulti(model, ps, "mobile", 'n', 'n', key1_ic, key2_ic, key1u_ic, key2u_ic, key3u_ic);
 	}
+
 
 	public static void testBassDeep(final CoreModel model, final PrintStream ps)
 	{
-		testLemmaDeep(model, "bass", 'n', 'n', Pronunciation.ipa("beɪs"), Pronunciation.ipa("bæs"), ps);
+		testLemmaDeep(model, ps, "bass", 'n', 'n', Pronunciation.ipa("beɪs"), Pronunciation.ipa("bæs"));
 	}
 
 	public static void testBassShallow(final CoreModel model, final PrintStream ps)
 	{
-		testLemmaShallow(model, "bass", 'n', 'n',  "-1",  "-2", ps);
+		testLemmaShallow(model, ps, "bass", 'n', 'n', "-1", "-2");
 	}
+
 
 	public static void testRowDeep(final CoreModel model, final PrintStream ps)
 	{
-		testLemmaDeep(model, "row", 'n', 'n', Pronunciation.ipa("ɹəʊ"), Pronunciation.ipa("ɹaʊ"), ps);
+		testLemmaDeep(model, ps, "row", 'n', 'n', Pronunciation.ipa("ɹəʊ"), Pronunciation.ipa("ɹaʊ"));
 	}
 
 	public static void testRowShallow(final CoreModel model, final PrintStream ps)
 	{
-		testLemmaShallow(model, "row", 'n', 'n', "-1", "-2", ps);
+		testLemmaShallow(model, ps, "row", 'n', 'n', "-1", "-2");
 	}
 
-	public static void testLemmaDeep(final CoreModel model, final String lemma, final char pos, final char type, final Pronunciation p1, final Pronunciation p2, final PrintStream ps)
+
+	public static void testCriticalDeep(final CoreModel model, final PrintStream ps)
+	{
+		testLemmaDeep(model, ps, "critical", 'a', 'a', Pronunciation.ipa("ˈkɹɪtɪkəl"));
+	}
+
+	public static void testCriticalPos(final CoreModel model, final PrintStream ps)
+	{
+		testLemmaPos(model, ps, "critical", 'a', 's', Pronunciation.ipa("ˈkɹɪtɪkəl"));
+	}
+
+	public static void testCriticalPWN(final CoreModel model, final PrintStream ps)
+	{
+		testLemmaPWN(model, ps, "critical", 'a');
+	}
+
+
+	public static void testLemmaDeep(final CoreModel model, final PrintStream ps, final String lemma, final char pos, final char type, final Pronunciation... pronunciations)
 	{
 		dump(lemma, pos, type, model, ps);
-
-		Key.OEWN key1 = new Key.OEWN(lemma, type, p1);
-		ps.println(key1.toLongString());
-		Lex lex1 = key1.apply(model.lexesByLemma);
-		ps.println("\t" + lex1);
-
-		Key.OEWN key2 = new Key.OEWN(lemma, type, p2);
-		ps.println(key2.toLongString());
-		Lex lex2 = key2.apply(model.lexesByLemma);
-		ps.println("\t" + lex2);
+		for (Pronunciation p : pronunciations)
+		{
+			Key.OEWN key1 = new Key.OEWN(lemma, type, p);
+			ps.println(key1.toLongString());
+			Lex lex1 = key1.apply(model.lexesByLemma);
+			ps.println("\t" + lex1);
+		}
 	}
 
-	public static void testLemmaShallow(final CoreModel model, final String lemma, final char pos, final char type, final String discriminant1, final String discriminant2, final PrintStream ps)
+	public static void testLemmaShallow(final CoreModel model, final PrintStream ps, final String lemma, final char pos, final char type, final String... discriminants)
 	{
 		dump(lemma, pos, type, model, ps);
-
-		Key.Shallow key1_sh = new Key.Shallow(lemma, type, discriminant1);
-		ps.println(key1_sh.toLongString());
-		Lex lex1_sh = key1_sh.apply(model.lexesByLemma);
-		ps.println("\t" + lex1_sh);
-
-		Key.Shallow key2_sh = new Key.Shallow(lemma, type, discriminant2);
-		ps.println(key2_sh.toLongString());
-		Lex lex2_sh = key2_sh.apply(model.lexesByLemma);
-		ps.println("\t" + lex2_sh);
+		for (String d : discriminants)
+		{
+			Key.Shallow key1_sh = new Key.Shallow(lemma, type, d);
+			ps.println(key1_sh.toLongString());
+			Lex lex1_sh = key1_sh.apply(model.lexesByLemma);
+			ps.println("\t" + lex1_sh);
+		}
 	}
 
-	public static void testCritical(final CoreModel model, final PrintStream ps)
+	public static void testLemmaPos(final CoreModel model, final PrintStream ps, final String lemma, final char pos, final char type, final Pronunciation... pronunciations)
 	{
-		String lemma = "critical";
-		Pronunciation pCritical = Pronunciation.ipa("ˈkɹɪtɪkəl");
-		dump(lemma, 'a', 's', model, ps);
+		dump(lemma, pos, type, model, ps);
+		for (Pronunciation p : pronunciations)
+		{
+			Key.Pos key_pos = new Key.Pos(lemma, pos, p);
+			ps.println(key_pos.toLongString());
+			Lex lex_pos = key_pos.apply(model.lexesByLemma); // first
+			ps.println("\t" + lex_pos);
+		}
+	}
 
-		Key.OEWN key = new Key.OEWN(lemma, 'a', pCritical);
-		ps.println(key.toLongString());
-		Lex lex = key.apply(model.lexesByLemma);
-		ps.println("\t" + lex);
+	public static void testLemmaPWN(final CoreModel model, final PrintStream ps, final String lemma, final char pos)
+	{
+		dump(lemma, pos, '\0', model, ps);
 
-		Key.Shallow key_sh = new Key.Shallow(lemma, 's', null);
-		ps.println(key_sh.toLongString());
-		Lex lex_sh = key_sh.apply(model.lexesByLemma);
-		ps.println("\t" + lex_sh);
-
-		Key.Pos key_pos = new Key.Pos(lemma, 'a', pCritical);
-		ps.println(key_pos.toLongString());
-		Lex lex_pos = key_pos.apply(model.lexesByLemma); // first
-		ps.println("\t" + lex_pos);
-
-		Key.PWN key_pwn = new Key.PWN(lemma, 'a');
+		Key.PWN key_pwn = new Key.PWN(lemma, pos);
 		ps.println(key_pwn.toLongString());
 		Lex[] lexes_pwn = key_pwn.apply(model.lexesByLemma);
 		for (Lex lex_pwn : lexes_pwn)
 		{
 			ps.println("\t" + lex_pwn);
+		}
+	}
+
+	@SafeVarargs
+	public static void testLemmaKeysMono(final CoreModel model, final PrintStream ps, final String lemma, final char pos, final char type, final Function<Map<String, List<Lex>>, Lex>... keys)
+	{
+		dump(lemma, pos, type, model, ps);
+		for (var k : keys)
+		{
+			ps.println(k);
+			Lex lex = k.apply(model.lexesByLemma);
+			ps.println("\t" + lex);
+		}
+	}
+
+	@SafeVarargs
+	public static void testLemmaKeysMulti(final CoreModel model, final PrintStream ps, final String lemma, final char pos, final char type, final Function<Map<String, List<Lex>>, Lex[]>... keys)
+	{
+		dump(lemma, pos, type, model, ps);
+		for (var k : keys)
+		{
+			ps.println(k);
+			Lex[] lexes = k.apply(model.lexesByLemma);
+			for (var lex : lexes)
+			{
+				ps.println("\t" + lex);
+			}
 		}
 	}
 
@@ -224,11 +201,14 @@ public class LibTestModelKeys
 			ps.println("\t" + lex);
 		}
 
-		ps.println("ALL LEMMAS WITH TYPE " + typeFilter + " " + lemma);
-		Lex[] lexes_t = Finder.getLexesHavingType(model.lexesByLemma, lemma, typeFilter);
-		for (var lex : lexes_t)
+		if (typeFilter != '\0')
 		{
-			ps.println("\t" + lex);
+			ps.println("ALL LEMMAS WITH TYPE " + typeFilter + " " + lemma);
+			Lex[] lexes_t = Finder.getLexesHavingType(model.lexesByLemma, lemma, typeFilter);
+			for (var lex : lexes_t)
+			{
+				ps.println("\t" + lex);
+			}
 		}
 
 		ps.println("ALL LEMMAS IGNORE CASE " + lemma);
@@ -245,11 +225,14 @@ public class LibTestModelKeys
 			ps.println("\t" + lex);
 		}
 
-		ps.println("ALL LEMMAS IGNORE CASE WITH TYPE " + typeFilter + " " + lemma);
-		Lex[] lexes_ut = Finder.getLcLexesHavingType(model.lexesByLemma, lemma, typeFilter);
-		for (var lex : lexes_ut)
+		if (typeFilter != '\0')
 		{
-			ps.println("\t" + lex);
+			ps.println("ALL LEMMAS IGNORE CASE WITH TYPE " + typeFilter + " " + lemma);
+			Lex[] lexes_ut = Finder.getLcLexesHavingType(model.lexesByLemma, lemma, typeFilter);
+			for (var lex : lexes_ut)
+			{
+				ps.println("\t" + lex);
+			}
 		}
 		ps.println("----------");
 	}

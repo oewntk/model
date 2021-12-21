cc352e027a612ac1ab231c8ca3a5d1e280123ff6
1313ou <1313ou@gmail.com>
2021-12-21 08:52:35 +0100

    merging map values

--- src/main/java/org/oewntk/model/MapFactory.java
+++ src/main/java/org/oewntk/model/MapFactory.java
@@ -5,9 +5,12 @@
 package org.oewntk.model;
 
 import java.util.Collection;
+import java.util.Locale;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.function.BinaryOperator;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 import static java.util.stream.Collectors.toMap;
 
@@ -17,29 +20,76 @@ public class MapFactory
 
 	// G E N E R I C   M A P   F A C T O R Y
 
+	private static class KeepMergingSupplier<V> implements Supplier<BinaryOperator<V>>
+	{
+		@Override
+		public BinaryOperator<V> get()
+		{
+			return (existing, replacement) -> {
+				if (existing.equals(replacement))
+				{
+					if (LOG_DUPLICATE_VALUES)
+					{
+						Tracing.psInfo.printf("[W] Duplicate values %s and %s, keeping first%n", existing, replacement);
+					}
+					//throw new IllegalArgumentException(existing + "," + replacement);
+				}
+				return existing;
+			};
+		}
+	}
+
+	private static class ReplaceMergingSupplier<V> implements Supplier<BinaryOperator<V>>
+	{
+		@Override
+		public BinaryOperator<V> get()
+		{
+			return (existing, replacement) -> {
+				if (existing.equals(replacement))
+				{
+					if (LOG_DUPLICATE_VALUES)
+					{
+						Tracing.psInfo.printf("[W] Duplicate values %s and %s, replacing first%n", existing, replacement);
+					}
+					//throw new IllegalArgumentException(existing + "," + replacement);
+				}
+				return replacement;
+			};
+		}
+	}
+
 	public static <K, V> Map<K, V> map(final Collection<V> things, final Function<V, K> groupingFunction)
+	{
+		var mergingFunction = new KeepMergingSupplier<V>().get();
+		return map(things, groupingFunction, mergingFunction);
+	}
+
+	public static <K, V> Map<K, V> map(final Collection<V> things, final Function<V, K> groupingFunction, final BinaryOperator<V> mergingFunction)
 	{
 		return things.stream() //
 				.collect(toMap(groupingFunction, //
 						Function.identity(), //
-						(existing, replacement) -> {
-							if (existing.equals(replacement))
-							{
-								if (LOG_DUPLICATE_VALUES)
-								{
-									Tracing.psInfo.printf("[W] Duplicate values %s and %s%n", existing, replacement);
-								}
-								//throw new IllegalArgumentException(existing + "," + replacement);
-							}
-							return existing;
-						}, TreeMap::new));
+						mergingFunction, //
+						TreeMap::new));
 	}
 
 	// G E N E R I C   M A P   F A C T O R Y
 
 	public static Map<String, Sense> sensesById(final Collection<Sense> senses)
 	{
-		return map(senses, Sense::getSensekey);
+		BinaryOperator<Sense> mergingFunction = (existing, replacement)-> {
+
+			Sense merged = replacement.getLex().isCased() ? (existing.getLex().isCased() ? existing : replacement) : existing;
+			if (existing.equals(replacement))
+			{
+				if (LOG_DUPLICATE_VALUES)
+				{
+					Tracing.psInfo.printf("[W] Duplicate values %s and %s, merging to %s%n", existing, replacement, merged);
+				}
+			}
+			return merged;
+		};
+		return map(senses, Sense::getSensekey, mergingFunction);
 	}
 
 	public static Map<String, Synset> synsetsById(final Collection<Synset> synsets)

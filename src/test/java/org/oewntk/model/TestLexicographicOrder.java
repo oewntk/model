586862a2b2d1929b1dd32bd4e443a2e6cf65f201
1313ou <1313ou@gmail.com>
2021-12-24 17:38:59 +0100

    test output

--- src/test/java/org/oewntk/model/TestLexicographicOrder.java
+++ src/test/java/org/oewntk/model/TestLexicographicOrder.java
@@ -6,6 +6,7 @@ package org.oewntk.model;
 
 import org.junit.Test;
 
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,27 +14,29 @@ import static org.junit.Assert.assertEquals;
 
 public class TestLexicographicOrder
 {
+	private static final PrintStream ps = !System.getProperties().containsKey("SILENT") ? Tracing.psInfo : Tracing.psNull;
+
 	@Test
 	public void testRow()
 	{
 		List<String> list = new ArrayList<>(List.of("alpha", "Alpha", "beta", "Beta", "gamma", "Gamma", "Alpha", "alPha", "Beta", "beTa", "Gamma", "gaMma"));
 
-		Tracing.psInfo.printf("%50s %s%n", "original", list);
+		ps.printf("%50s %s%n", "original", list);
 
 		list.sort(String::compareTo);
-		Tracing.psInfo.printf("%50s %s%n", "String::compareTo", list);
+		ps.printf("%50s %s%n", "String::compareTo", list);
 		assertEquals("[Alpha, Alpha, Beta, Beta, Gamma, Gamma, alPha, alpha, beTa, beta, gaMma, gamma]", list.toString());
 
 		list.sort(String::compareToIgnoreCase);
-		Tracing.psInfo.printf("%50s %s%n", "String::compareToIgnoreCase", list);
+		ps.printf("%50s %s%n", "String::compareToIgnoreCase", list);
 		assertEquals("[Alpha, Alpha, alPha, alpha, Beta, Beta, beTa, beta, Gamma, Gamma, gaMma, gamma]", list.toString());
 
 		list.sort(LexicographicOrder.upperFirst);
-		Tracing.psInfo.printf("%50s %s%n", "upperFirst", list);
+		ps.printf("%50s %s%n", "upperFirst", list);
 		assertEquals("[Alpha, Alpha, alPha, alpha, Beta, Beta, beTa, beta, Gamma, Gamma, gaMma, gamma]", list.toString());
 
 		list.sort(LexicographicOrder.lowerFirst);
-		Tracing.psInfo.printf("%50s %s%n", "lowerFirst", list);
+		ps.printf("%50s %s%n", "lowerFirst", list);
 		assertEquals("[alpha, alPha, Alpha, Alpha, beta, beTa, Beta, Beta, gamma, gaMma, Gamma, Gamma]", list.toString());
 	}
 }

05bc703343653d446ecd83b844108a78471f35a2
1313ou <1313ou@gmail.com>
2022-01-05 08:00:55 +0100

    revise sense decreasing order

--- src/main/java/org/oewntk/model/SenseGroupings.java
+++ src/main/java/org/oewntk/model/SenseGroupings.java
@@ -116,25 +116,59 @@ public class SenseGroupings
 
 	public static final Comparator<Sense> byDecreasingTagCount = (s1, s2) -> {
 
+		// sense are equal
 		if (s1.equals(s2))
 		{
 			return 0;
 		}
+		// tag count
 		int c1 = s1.getIntTagCount();
 		int c2 = s2.getIntTagCount();
-		int cmp1 = Integer.compare(c1, c2);
-		if (cmp1 != 0)
+		int cmpTagCount = Integer.compare(c1, c2);
+		if (cmpTagCount != 0)
 		{
-			return -cmp1;
+			// tag counts differ
+			return -cmpTagCount;
 		}
+		// same tag count, possibly zero
 		String lemma1 = s1.getLemma();
 		String lemma2 = s2.getLemma();
-		if (lemma1.equals(lemma2))
+		/*
+		if (!lemma1.equalsIgnoreCase(lemma2))
 		{
-			return Integer.compare(s1.getLexIndex(), s2.getLexIndex());
+			throw new IllegalArgumentException(lemma1 + "-"+ lemma2);
 		}
-		// upper-case first
-		return lemma1.compareTo(lemma2);
+		*/
+
+		// type a before s
+		if (s1.getType() != s2.getType())
+		{
+			return Character.compare(s1.getType(), s2.getType());
+		}
+
+		// different lemmas, upper-case first
+		int cmpCase = lemma1.compareTo(lemma2);
+		if (cmpCase != 0)
+		{
+			// different lemmas, upper-case first
+			return cmpCase;
+		}
+
+		// index in lex
+		int cmpLexIndex = Integer.compare(s1.getLexIndex(), s2.getLexIndex());
+		if (cmpLexIndex != 0)
+		{
+			// lex indexes differ
+			return cmpLexIndex;
+		}
+
+		// compare sensekey
+		int cmpSensekey = s1.getSensekey().compareTo(s2.getSensekey());
+		if (cmpSensekey != 0)
+		{
+			return cmpSensekey;
+		}
+		throw new IllegalArgumentException(s1 + "-" + s2);
 	};
 
 	public static <K> void dumpSensesByDecreasingTagCount(List<Sense> senses, final PrintStream ps)

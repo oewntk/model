549825199e747e19e21843914e4a50189cd2d944
1313ou <1313ou@gmail.com>
2024-04-23 21:32:27 +0200

    senses

--- src/main/java/org/oewntk/model/Lex.kt
+++ src/main/java/org/oewntk/model/Lex.kt
@@ -21,145 +21,97 @@ import java.util.*
  *
  */
 class Lex(
-    /**
-     * Lemma written form
-     */
-    @JvmField val lemma: String,
+	/**
+	 * Lemma written form
+	 */
+	@JvmField val lemma: String,
 
-    /**
-     * Code
-     */
-    code: String,
+	/**
+	 * Code
+	 */
+	code: String,
 
-    /**
-     * Source file
-     */
-    val source: String?
+	/**
+	 * Source file
+	 */
+	val source: String?
 
 ) : Serializable //, Comparable<Lex>
 {
-    /**
-     * Lemma lower-cased written form
-     */
-    val lCLemma: String
-        get() = lemma.lowercase()
+	/**
+	 * Lemma lower-cased written form
+	 */
+	val lCLemma: String
+		get() = lemma.lowercase()
 
-    /**
-     * Whether lemma has uppercase
-     */
-    val isCased: Boolean
-        get() = lemma != lCLemma
+	/**
+	 * Whether lemma has uppercase
+	 */
+	val isCased: Boolean
+		get() = lemma != lCLemma
 
-    /**
-     * Synset type ss_type {n, v, a, r, s}
-     */
-    @JvmField
-    val type: Char = code[0]
+	/**
+	 * Synset type ss_type {n, v, a, r, s}
+	 */
+	@JvmField
+	val type: Char = code[0]
 
-    /**
-     * Part-of-speech (sme as part-of-speech except for satellite adjective)
-     */
-    val partOfSpeech: Char
-        get() = if (this.type == 's') 'a' else this.type
+	/**
+	 * Part-of-speech (sme as part-of-speech except for satellite adjective)
+	 */
+	val partOfSpeech: Char
+		get() = if (this.type == 's') 'a' else this.type
 
-    /**
-     * Discriminant amongst same-type lexes appended to type that distinguishes same-type lexes (because of pronunciation or morphological forms )
-     * Current values are '-1','-2'
-     */
-    @JvmField
-    val discriminant: String? = if (code.length > 1) code.substring(1) else null
+	/**
+	 * Discriminant amongst same-type lexes appended to type that distinguishes same-type lexes (because of pronunciation or morphological forms )
+	 * Current values are '-1','-2'
+	 */
+	@JvmField
+	val discriminant: String? = if (code.length > 1) code.substring(1) else null
 
-    /**
-     * Senses
-     */
-    lateinit var senses: MutableList<Sense>
+	/**
+	 * Senses
+	 */
+	lateinit var senses: MutableList<Sense>
 
-    /**
-     * Set senses
-     *
-     * @param senses senses
-     */
-    fun setSenses(senses: Array<Sense>) {
-        this.senses.addAll(listOf(*senses))
-    }
+	/**
+	 * Morphological forms
+	 */
+	var forms: Array<out String>? = null
 
-    /**
-     * Add sense to the list fo senses
-     *
-     * @param sense sense
-     */
-    fun addSense(sense: Sense) {
-        senses.add(sense)
-    }
+	/**
+	 * Pronunciations
+	 */
+	var pronunciations: Array<Pronunciation>? = null
 
-    /**
-     * Senses as a set
-     */
-    val sensesAsSet: Set<Sense>
-        get() = LinkedHashSet(senses)
+	// stringify
 
-    /**
-     * Morphological forms
-     */
-    var forms: Array<out String>? = null
-        private set
+	override fun toString(): String {
+		val pronunciationsStr = Formatter.join(pronunciations, ",")
+		val sensesStr = Formatter.join(senses, ",")
+		return "$lemma $type${discriminant ?: ""} $pronunciationsStr {$sensesStr}"
+	}
 
-    /**
-     * Set morphological forms
-     *
-     * @param forms morphological forms
-     * @return this
-     */
-    fun setForms(forms: Array<String>?): Lex {
-        this.forms = forms
-        return this
-    }
+	// identify
 
-    /**
-     * Pronunciations
-     */
-    var pronunciations: Array<Pronunciation>? = null
-        private set
+	override fun equals(other: Any?): Boolean {
+		throw UnsupportedOperationException("Either compare values using 'sensesAsSet' or keys using 'Key.OEWN.of', 'Key.PWN.of', etc")
+	}
 
-    /**
-     * Set pronunciations
-     *
-     * @param pronunciations pronunciations
-     * @return this
-     */
-    fun setPronunciations(pronunciations: Array<Pronunciation>?): Lex {
-        this.pronunciations = pronunciations
-        return this
-    }
+	override fun hashCode(): Int {
+		var result = Objects.hash(lemma, type, discriminant)
+		result = 31 * result + pronunciations.contentHashCode()
+		return result
+	}
 
-    // stringify
+	companion object {
 
-    override fun toString(): String {
-        val pronunciationsStr = Formatter.join(pronunciations, ",")
-        val sensesStr = Formatter.join(senses, ",")
-        return "$lemma $type${discriminant ?: ""} $pronunciationsStr {$sensesStr}"
-    }
+		// ordering, this is used when sorted maps are made
 
-    // identify
-
-    override fun equals(other: Any?): Boolean {
-        throw UnsupportedOperationException("Either compare values using 'sensesAsSet' or keys using 'Key.OEWN.of', 'Key.PWN.of', etc")
-    }
-
-    override fun hashCode(): Int {
-        var result = Objects.hash(lemma, type, discriminant)
-        result = 31 * result + pronunciations.contentHashCode()
-        return result
-    }
-
-    companion object {
-
-        // ordering, this is used when sorted maps are made
-
-        val comparatorByKeyOEWN: Comparator<Lex> = Comparator { thisLex: Lex, thatLex: Lex ->
-            val thisKey = W_P_A.of_t(thisLex)
-            val thatKey = W_P_A.of_t(thatLex)
-            thisKey.compareTo(thatKey)
-        }
-    }
+		val comparatorByKeyOEWN: Comparator<Lex> = Comparator { thisLex: Lex, thatLex: Lex ->
+			val thisKey = W_P_A.of_t(thisLex)
+			val thatKey = W_P_A.of_t(thatLex)
+			thisKey.compareTo(thatKey)
+		}
+	}
 }

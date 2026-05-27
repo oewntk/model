56864ce17c58f4ff973d353fd1ac6221852aeefe
1313ou <1313ou@gmail.com>
2026-05-27 07:42:53 +0200

    checker rewrite

--- src/main/java/org/oewntk/model/Checker.kt
+++ src/main/java/org/oewntk/model/Checker.kt
@@ -3,41 +3,105 @@ package org.oewntk.model
 /**
  * Check model
  */
-fun <M : CoreModel> M.check(verbose: Boolean = true): M {
-    checkMembers(verbose = verbose)
-    if (verbose) Tracing.psErr.println("[I] Check synset relation targets")
-    checkSynsetRelationTargets(verbose = verbose)
-    if (verbose) Tracing.psErr.println("[I] Check sense relation targets")
-    checkSenseRelationTargets(verbose = verbose)
-    if (verbose) Tracing.psErr.println("[I] Check sense relation targets")
-    checkMembersSenses(verbose = verbose)
+fun <M : CoreModel> M.check(throws: Boolean = true, verbose: Boolean = false): M {
+    if (verbose) Tracing.psErr.println("[I] Lex value duplicates")
+    checkLexValueDuplicates(throws = throws, verbose = verbose)
+    if (verbose) Tracing.psErr.println("[I] Lex key duplicates")
+    checkLexKeyDuplicates(throws = throws, verbose = verbose)
+
+    if (verbose) Tracing.psErr.println("[I] Sense reference")
+    checkSenseReference(throws = throws, verbose = verbose)
+
+    if (verbose) Tracing.psErr.println("[I] Synset members")
+    checkMembers(throws = throws, verbose = verbose)
+
+    if (verbose) Tracing.psErr.println("[I] Synset relation targets")
+    checkSynsetRelationTargets(throws = throws, verbose = verbose)
+    if (verbose) Tracing.psErr.println("[I] Sense relation targets")
+    checkSenseRelationTargets(throws = throws, verbose = verbose)
+    return this
+}
+
+// L E X
+
+/**
+ * Check lex value duplicates
+ */
+fun <M : CoreModel> M.checkLexValueDuplicates(throws: Boolean = false, verbose: Boolean = true): M {
+    val duplicates = lexes.groupBy { it.value }
+        .filter { it.value.size > 1 }
+        .keys
+    if (duplicates.isNotEmpty()) {
+        val state = "duplicate lex values:\n ${duplicates.joinToString("\n")}"
+        if (throws) throw IllegalStateException(state)
+        if (verbose) Tracing.psErr.println("[E] $state")
+    }
+    val count = duplicates.size
+    Tracing.psErr.println("[${if (count == 0) "I" else "E"}] $count lexes have value duplicate(s)")
     return this
 }
 
 /**
+ * Check lex key duplicates
+ */
+fun <M : CoreModel> M.checkLexKeyDuplicates(throws: Boolean = false, verbose: Boolean = true): M {
+    val duplicates = lexes.groupBy { it.key }
+        .filter { it.value.size > 1 }
+        .keys
+    if (duplicates.isNotEmpty()) {
+        val state = "duplicate lex keys:\n ${duplicates.joinToString("\n")}"
+        if (throws) throw IllegalStateException(state)
+        if (verbose) Tracing.psErr.println("[E] $state")
+    }
+    val count = duplicates.size
+    Tracing.psErr.println("[${if (count == 0) "I" else "E"}] $count lexes have key duplicate(s)")
+    return this
+}
+
+// S E N S E
+
+fun <M : CoreModel> M.checkSenseReference(throws: Boolean = false, verbose: Boolean = true): M {
+    var count = 0
+    senses.forEach { sense ->
+        if (synsetFinder(sense.synsetId) == null) {
+            count++
+            val state = "${sense.senseKey} has non-existing target synset ${sense.synsetId}"
+            if (throws) throw IllegalStateException(state)
+            if (verbose) Tracing.psErr.println("[E] $state")
+        }
+    }
+    Tracing.psErr.println("[${if (count == 0) "I" else "E"}] $count senses have no or non-existing synset target")
+    return this
+}
+
+// R E L A T I O N S
+
+/**
  * Check synset relation targets
  */
-fun <M : CoreModel> M.checkSynsetRelationTargets(verbose: Boolean = true): M {
+fun <M : CoreModel> M.checkSynsetRelationTargets(throws: Boolean = false, verbose: Boolean = true): M {
     var count = 0
-    if (synsetsById != null) {
-        for ((sourceSynsetId, sourceSynset) in synsetsById) {
-            if (!sourceSynset.relations.isNullOrEmpty()) {
-                sourceSynset.relations!!.forEach { (rel, targetSynsetIds) ->
-                    if (targetSynsetIds.isNotEmpty()) {
-                        for (targetSynsetId in targetSynsetIds) {
-                            if (targetSynsetId[0] != 'Q' && synsetFinder(targetSynsetId) == null) {
-                                count++
-                                if (verbose) Tracing.psErr.println("[E] non-existing target $targetSynsetId of synset relation $rel($sourceSynsetId)")
-                            }
+    synsets
+        .filter { !it.relations.isNullOrEmpty() }
+        .forEach {
+            it.relations?.forEach { (rel, targetSynsetIds) ->
+                if (targetSynsetIds.isNotEmpty()) {
+                    for (targetSynsetId in targetSynsetIds) {
+                        if (targetSynsetId[0] != 'Q' && synsetFinder(targetSynsetId) == null) {
+                            count++
+                            val state = "non-existing target $targetSynsetId of synset relation $rel(${it.synsetId})"
+                            if (throws) throw IllegalStateException(state)
+                            if (verbose) Tracing.psErr.println("[E] $state")
                         }
-                    } else {
-                        count++
-                        if (verbose) Tracing.psErr.println("[E] no target of synset relation $rel($sourceSynsetId)")
                     }
+                } else {
+                    count++
+                    val state = "no target of synset relation $rel(${it.synsetId})"
+                    if (throws) throw IllegalStateException(state)
+                    if (verbose) Tracing.psErr.println("[E] $state")
                 }
             }
         }
-    }
     Tracing.psErr.println("[${if (count == 0) "I" else "E"}] $count synset relations have no or non-existing target")
     return this
 }
@@ -45,92 +109,100 @@ fun <M : CoreModel> M.checkSynsetRelationTargets(verbose: Boolean = true): M {
 /**
  * Check sense relation targets
  */
-fun <M : CoreModel> M.checkSenseRelationTargets(verbose: Boolean = true): M {
+fun <M : CoreModel> M.checkSenseRelationTargets(throws: Boolean = false, verbose: Boolean = true): M {
     var count = 0
-    if (sensesById != null) {
-        for ((sourceSenseId, sourceSense) in sensesById) {
-            if (!sourceSense.relations.isNullOrEmpty()) {
-                sourceSense.relations!!.forEach { (rel, targetSynsetIds) ->
-                    if (targetSynsetIds.isNotEmpty()) {
-                        for (targetSynsetId in targetSynsetIds) {
-                            if (senseFinder(targetSynsetId) == null) {
-                                count++
-                                if (verbose) Tracing.psErr.println("[E] non-existing target $targetSynsetId of sense relation $rel($sourceSenseId)")
-                            }
+    senses
+        .filter { !it.relations.isNullOrEmpty() }
+        .forEach {
+            it.relations?.forEach { (rel, targetSenseKeys) ->
+                if (targetSenseKeys.isNotEmpty()) {
+                    for (targetSenseKey in targetSenseKeys) {
+                        if (senseFinder(targetSenseKey) == null) {
+                            count++
+                            val state = "non-existing target $targetSenseKey of sense relation $rel(${it.senseKey})"
+                            if (throws) throw IllegalStateException(state)
+                            if (verbose) Tracing.psErr.println("[E] $state")
                         }
-                    } else {
-                        count++
-                        if (verbose) Tracing.psErr.println("[E] no target of sense relation $rel($sourceSenseId)")
                     }
+                } else {
+                    count++
+                    val state = "no target of sense relation $rel(${it.senseKey})"
+                    if (throws) throw IllegalStateException(state)
+                    if (verbose) Tracing.psErr.println("[E] $state")
                 }
             }
         }
-    }
     Tracing.psErr.println("[${if (count == 0) "I" else "E"}] $count sense relations have no or non-existing target")
     return this
 }
 
+// S Y N S E T  M E M B E R S
+
 /**
  * Check synset members
  */
-fun <M : CoreModel> M.checkMembers(verbose: Boolean = true): M {
-    checkMembersDuplicates(verbose = verbose)
-    checkMembersReference(verbose = verbose)
-    checkMembersSenses(verbose = verbose)
+fun <M : CoreModel> M.checkMembers(throws: Boolean = false, verbose: Boolean = true): M {
+    checkMembersDuplicates(throws = throws, verbose = verbose)
+    checkMembersReference(throws = throws, verbose = verbose)
+    checkMembersSenses(throws = throws, verbose = verbose)
     return this
 }
 
 /**
  * Check synset members
  */
-fun <M : CoreModel> M.checkMembersDuplicates(verbose: Boolean = true): M {
+fun <M : CoreModel> M.checkMembersDuplicates(throws: Boolean = false, verbose: Boolean = true): M {
     var count = 0
-    for (synset in synsets) {
+    synsets.forEach { synset ->
         val duplicates = synset.members.groupBy { it }
             .filter { it.value.size > 1 }
             .keys
         if (duplicates.isNotEmpty()) {
             count++
-            if (verbose) Tracing.psErr.println("[E] duplicate synset ${synset.synsetId} members: $duplicates {${synset.members.joinToString()}}")
+            val state = "duplicate synset ${synset.synsetId} members: $duplicates {${synset.members.joinToString()}}"
+            if (throws) throw IllegalStateException(state)
+            if (verbose) Tracing.psErr.println("[E] $state")
         }
     }
-    Tracing.psErr.println("[${if (count == 0) "I" else "E"}] $count synsets have member duplicate(s)")
+    Tracing.psErr.println("[${if (count == 0) "I" else "E"}]\t$count synsets have member duplicate(s)")
     return this
 }
 
 /**
  * Check synset members
  */
-fun <M : CoreModel> M.checkMembersReference(verbose: Boolean = true): M {
+fun <M : CoreModel> M.checkMembersReference(throws: Boolean = false, verbose: Boolean = true): M {
     var count = 0
-    for (synset in synsets) {
+    synsets.forEach { synset ->
         val orphans = synset.members.filter { lexFinder(it) == null }.toList()
         if (orphans.isNotEmpty()) {
             count++
-            if (verbose) Tracing.psErr.println("[E] members of synset ${synset.synsetId} with members {${synset.members.joinToString()}} have no entry: $orphans")
+            val state = "members of synset ${synset.synsetId} with members {${synset.members.joinToString()}} have no entry: $orphans"
+            if (throws) throw IllegalStateException(state)
+            if (verbose) Tracing.psErr.println("[E] $state")
         }
     }
-    Tracing.psErr.println("[${if (count == 0) "I" else "E"}] $count synsets have member(s) without entries")
+    Tracing.psErr.println("[${if (count == 0) "I" else "E"}]\t$count synsets have member(s) without entries")
     return this
 }
 
 /**
  * Check synset members
  */
-fun <M : CoreModel> M.checkMembersSenses(verbose: Boolean = true): M {
+fun <M : CoreModel> M.checkMembersSenses(throws: Boolean = false, verbose: Boolean = true): M {
     var count = 0
-    for (synset in synsets) {
-        if ("00821893-n" == synset.synsetId)
-            Tracing.psInfo.println()
-        for (member in synset.members) {
+    synsets.forEach { synset ->
+        synset.members.forEach { member ->
             try {
                 synset.findSenseOf(member, lexResolver, senseResolver)
             } catch (ise: IllegalStateException) {
                 count++
-                if (verbose) Tracing.psErr.println("[E] members of synset ${synset.synsetId} with members {${synset.members.joinToString()}} have no found sense for '$member' ${ise.message}")
+                val state = "members of synset ${synset.synsetId} with members {${synset.members.joinToString()}} have no found sense for '$member' ${ise.message}"
+                if (throws) throw IllegalStateException(state)
+                if (verbose) Tracing.psErr.println("[E] $state")
             }
         }
     }
-    Tracing.psErr.println("[${if (count == 0) "I" else "E"}] $count synsets have member(s) with non found sense")
+    Tracing.psErr.println("[${if (count == 0) "I" else "E"}]\t$count synsets have member(s) with non found sense")
     return this
 }

4718d0b1bb14be1433f7ceee06933452b0fb0a0d
1313ou <1313ou@gmail.com>
2026-05-28 12:16:07 +0200

    sensekey generator to use discriminated idx for lowercased sensekeys

--- src/main/java/org/oewntk/model/SenseKeys.kt
+++ src/main/java/org/oewntk/model/SenseKeys.kt
@@ -62,11 +62,11 @@ object SenseKeys {
     fun SynsetType.toPosNum(): Int = ordinal + 1
 
     fun generateSenseKey(lemma: Lemma, synset: Synset, idx: Int): SenseKey {
-        lemma.lowercase(Locale.ENGLISH).escapeForSenseKey()
+        val discriminant = if (lemma[0].isUpperCase()) 1 else 0
         val escapedLemma = lemma.lowercase(Locale.ENGLISH).escapeForSenseKey()
         val ssType = synset.type.toPosNum()
         val lexfileNum = "%02d".format(synset.lexfile.toLexFileNum())
-        val lexfileIdx = "%02d".format(idx)
-        return "$escapedLemma%$ssType:$lexfileNum:$lexfileIdx::"
+        val lexfileIdx = "%02d".format(idx+ discriminant)
+        return "$escapedLemma%$ssType:$lexfileNum:$lexfileIdx::99" //TODO
     }
 }

664f9304aecd4b0e482b20e5f2f451417a9a74bd
1313ou <1313ou@gmail.com>
2026-06-02 14:06:40 +0200

    subset of model data

--- src/test/java/org/oewntk/model/LibModelSubset.kt
+++ src/test/java/org/oewntk/model/LibModelSubset.kt
@@ -16,4 +16,10 @@ object LibModelSubset {
     fun CoreModel.synsetSubset(from: Int = (1000..100000).random(), howMany: Int = 100): Sequence<Synset> = subsetOf(synsets, howMany = howMany)
 
     fun CoreModel.senseSubset(from: Int = (1000..100000).random(), howMany: Int = 100): Sequence<Sense> = subsetOf(senses, howMany = howMany)
+
+    fun CoreModel.subset(from: Int = (1000..100000).random(), howMany: Int = 100): Triple<Sequence<Lex>, Sequence<Synset>, Sequence<Sense>> = Triple(
+        lexSubset( from = from, howMany = howMany),
+        synsetSubset( from = from, howMany = howMany),
+        senseSubset( from = from, howMany = howMany),
+    )
 }

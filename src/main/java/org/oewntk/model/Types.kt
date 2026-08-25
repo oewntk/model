e1699ce6de2a332397930c0392805f945a693972
1313ou <1313ou@gmail.com>
2026-08-25 09:03:03 +0200

    export id regexes to file

--- src/main/java/org/oewntk/model/Types.kt
+++ src/main/java/org/oewntk/model/Types.kt
@@ -207,37 +207,8 @@ enum class CategoryImpl(val value: Char) {
     abstract fun toPartOfSpeech(): PartOfSpeechImpl
 }
 
-
 /**
  * Example
  */
 @kotlinx.serialization.Serializable
 data class ExampleImpl(val text: String, val source: String? = null) : Serializable
-
-const val SYNSET_ID_RE = "\\d{8}-[nvars]"
-
-const val BASE_LEMMA_CHARS_RE = "a-zA-Z\\u00C0-\\u00D6\\u00D8-\\u00F60-9\\-+.,:!/'"
-const val LEMMA_CHARS_RE = "$BASE_LEMMA_CHARS_RE "
-const val ESC_LEMMA_CHARS_RE = "${BASE_LEMMA_CHARS_RE}_"
-
-const val LEMMA_RE = "[${LEMMA_CHARS_RE}]+"
-
-const val LEXID_RE = "$LEMMA_RE,[nvar]-?[\\d]?"
-
-const val SENSEKEY_RE = "(?!$SYNSET_ID_RE$)[${ESC_LEMMA_CHARS_RE}]+%\\d+:\\d+:\\d+:[${ESC_LEMMA_CHARS_RE}]*:\\d*"
-
-val synsetIdRegex = "^$SYNSET_ID_RE$".toRegex()
-
-val senseKeyRegex = "^$SENSEKEY_RE$".toRegex()
-
-val lexIdRegex = "^$LEXID_RE$".toRegex()
-
-val lemmaRegex = "^$LEMMA_RE$".toRegex()
-
-fun String.isSynsetId(): Boolean = synsetIdRegex.matches(this)
-
-fun String.isSenseKey(): Boolean = senseKeyRegex.matches(this)
-
-fun String.isLemma(): Boolean = lemmaRegex.matches(this)
-
-fun String.isLexId(): Boolean = lexIdRegex.matches(this)
